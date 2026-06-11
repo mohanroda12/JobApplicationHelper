6f268aaf7f004e8995eb68563e4172bacc456f71
Mohan <mohanroda12@gmail.com>
2026-06-11 21:25:58 +0100

    Added basic nav bar with home link and signup/login links

--- src/main/java/com/jobhelp/jobapplicationhelper/models/LoginModel.java
+++ src/main/java/com/jobhelp/jobapplicationhelper/models/LoginModel.java
@@ -11,7 +11,6 @@ public class LoginModel {
     private String email;
 
     @Size(min=8, max=15, message="Password must be between 8 and 15 characters")
-
     private String password;
 
     public LoginModel() {
