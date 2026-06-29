7462e8d9dab569c0acffb242ad5c475642d03045
Mohan <mohanroda12@gmail.com>
2026-06-29 23:20:44 +0100

    Created front end for creating new application and linked it to backend

--- src/main/java/com/jobhelp/jobapplicationhelper/controllers/JobApplicationController.java
+++ src/main/java/com/jobhelp/jobapplicationhelper/controllers/JobApplicationController.java
@@ -1,11 +1,17 @@
 package com.jobhelp.jobapplicationhelper.controllers;
 
+import com.jobhelp.jobapplicationhelper.models.ApplicationStatus;
 import com.jobhelp.jobapplicationhelper.models.JobApplication;
+import com.jobhelp.jobapplicationhelper.models.Login;
 import com.jobhelp.jobapplicationhelper.services.JobApplicationServiceInterface;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,7 +24,7 @@ public class JobApplicationController {
     @Autowired
     JobApplicationServiceInterface jobApplication;
 
-    @GetMapping("")
+    @GetMapping("/showAll")
     public String showAllApplications(Model model) {
 
         List<JobApplication> applications = jobApplication.getJobApplications();
@@ -27,4 +33,22 @@ public class JobApplicationController {
         return "applications";
     }
 
+    @GetMapping("/addNew")
+    public String addNewApplication(Model model) {
+
+        model.addAttribute("application", new JobApplication());
+        model.addAttribute("statuses", ApplicationStatus.values());
+
+        return "addNewApplication";
+    }
+
+
+    @PostMapping("/saveNew")
+    public String addNewApplication(@Valid JobApplication newApplication, BindingResult bindingResult, Model model) {
+        newApplication.setId(null);
+        jobApplication.addOne(newApplication);
+        return "redirect:/applications/showAll";
+    }
 }
+
+
--- src/main/java/com/jobhelp/jobapplicationhelper/data/JobApplicationDataService.java
+++ src/main/java/com/jobhelp/jobapplicationhelper/data/JobApplicationDataService.java
@@ -5,7 +5,6 @@ import com.jobhelp.jobapplicationhelper.models.JobApplicationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
