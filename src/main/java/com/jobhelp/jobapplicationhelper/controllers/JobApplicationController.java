8444230532c3895243013372422da335e4a85358
Mohan <mohanroda12@gmail.com>
2026-06-30 16:34:47 +0100

    Created frontend and connected to backend for searching for role of job applications

--- src/main/java/com/jobhelp/jobapplicationhelper/controllers/JobApplicationController.java
+++ src/main/java/com/jobhelp/jobapplicationhelper/controllers/JobApplicationController.java
@@ -2,7 +2,7 @@ package com.jobhelp.jobapplicationhelper.controllers;
 
 import com.jobhelp.jobapplicationhelper.models.ApplicationStatus;
 import com.jobhelp.jobapplicationhelper.models.JobApplication;
-import com.jobhelp.jobapplicationhelper.models.Login;
+import com.jobhelp.jobapplicationhelper.models.SearchModel;
 import com.jobhelp.jobapplicationhelper.services.JobApplicationServiceInterface;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,17 +23,18 @@ public class JobApplicationController {
     @Autowired
     JobApplicationServiceInterface jobApplication;
 
-    @GetMapping("/showAll")
+    @GetMapping("")
     public String showAllApplications(Model model) {
 
         List<JobApplication> applications = jobApplication.getJobApplications();
         model.addAttribute("applications", applications);
+        model.addAttribute("searchModel", new SearchModel());
 
         return "applications";
     }
 
     @GetMapping("/addNew")
-    public String addNewApplication(Model model) {
+    public String addNewApplicationForm(Model model) {
 
         model.addAttribute("application", new JobApplication());
         model.addAttribute("statuses", ApplicationStatus.values());
@@ -42,12 +42,23 @@ public class JobApplicationController {
         return "addNewApplication";
     }
 
-
     @PostMapping("/saveNew")
     public String addNewApplication(@Valid JobApplication newApplication, BindingResult bindingResult, Model model) {
         newApplication.setId(null);
         jobApplication.addOne(newApplication);
-        return "redirect:/applications/showAll";
+        return "redirect:/applications";
+    }
+
+    @PostMapping("/search/role")
+    public String searchByRole(@Valid SearchModel searchModel, BindingResult bindingResult, Model model) {
+
+        String searchTerm = searchModel.getSearchTerm();
+
+        //Filter by search term
+        List<JobApplication> applications = jobApplication.searchByRole(searchTerm);
+        model.addAttribute("applications", applications);
+
+        return "applications";
     }
 }
 
---
+++ src/main/java/com/jobhelp/jobapplicationhelper/models/SearchModel.java
@@ -0,0 +1,17 @@
+package com.jobhelp.jobapplicationhelper.models;
+
+public class SearchModel {
+
+    String searchTerm;
+
+    public SearchModel() {
+    }
+
+    public String getSearchTerm() {
+        return searchTerm;
+    }
+
+    public void setSearchTerm(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
+}
